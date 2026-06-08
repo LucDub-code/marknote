4bf7c1bc816dc58706a4c5f85de768f03933d0ba
LucDub-code <contact@lucasdubeau.dev>
2026-06-09 01:14:08 +0200

    feat(header): add app header with menu button and component scaffolding

---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import MenuButton from './header/MenuButton.vue'
+</script>
+
+<template>
+  <header>
+    <MenuButton />
+  </header>
+</template>
+
+<style scoped lang="scss">
+  header {
+    background-color: var(--slate-800);
+    width: 100%;
+    height: 72px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between; 
+  }
+</style>
---
+++ client/src/components/header/MenuButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="menu-button">
+    <img src="../../assets/icons/open-menu.svg" alt="Menu" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .menu-button {
+    background-color: var(--slate-700);
+    width: 72px;
+    height: 100%;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
