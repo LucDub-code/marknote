c5018284e8f878393c6fc0a82345cd5199e34123
LucDub-code <contact@lucasdubeau.dev>
2026-06-09 05:25:06 +0200

    feat(styles): add Roboto fonts via Fontsource and text-preset utility classes

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,4 +3,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.scss'
 
+// Roboto : light, regular, medium
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+
+// Roboto Slab : light, regular, bold
+import '@fontsource/roboto-slab/300.css'
+import '@fontsource/roboto-slab/400.css'
+import '@fontsource/roboto-slab/700.css'
+
+// Roboto Mono : regular
+import '@fontsource/roboto-mono/400.css'
+
 createApp(App).use(createPinia()).mount('#app')
